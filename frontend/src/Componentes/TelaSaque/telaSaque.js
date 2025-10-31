724d4f081b186db366ad873f3a6e99512e19fcb6
João Gnoatto <joaogabrielgnoatto2@gmail.com>
2025-10-31 10:18:36 -0300

    REACT E CSS
    CRIAÇÃO DE TELAS
     - Tela de Saque

--- frontend/src/Componentes/TelaSaque/telaSaque.js
+++ frontend/src/Componentes/TelaSaque/telaSaque.js
@@ -1,9 +1,36 @@
 import SideBar from "../SideBar/sideBar"
+import MainButton from "../MainButton/mainButton"
+import SaldoDisponivel from "../SaldoDisponível/saldoDisponivel"
+import "./telaSaque.css"
+import LimiteSaque from "../LimiteSaque/limiteSaque"
 
 function telaSaque(){
     return(
         <div>
             <SideBar/>
+           <div className="functions-container">
+                <div className="saldo-card">
+                    <h2>Quanto você deseja sacar?</h2>
+                     <section className="linha">
+                        <p className="saldo-text">R$</p>
+                        <input type="text" placeholder="0,00"/>
+                     </section>
+                </div>
+                <div className="functions-card-deposito">
+                    <p>Deseja confirmar seu saque?</p>
+                    <MainButton
+                        texto="CONFIRMAR"
+                        onClick="/confirmar-saque"
+                    />
+                </div>
+            </div>
+            <section className="limite-saldo">
+                <LimiteSaque saque="1.000,00"/>
+                <div className="saldo-texto">
+                    <SaldoDisponivel saldo="10.000,00"/>
+                </div>
+                
+            </section>
         </div>
     )
 }
